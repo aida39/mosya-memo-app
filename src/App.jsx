255ec46e54aab925c6d46e3090af9f7b4339d168
aida39 <alma2013mtk@gmail.com>
2024-09-03 15:04:40 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Form from "./Form.jsx";
+import SideMenu from "./SideMenu.jsx";
+
+
+function App() {
+
+  return (
+    <div className='app'>
+      <SideMenu />
+      <Form />
+    </div>
+  )
+}
+
+export default App
